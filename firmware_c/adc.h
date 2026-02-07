e0cca0b17a4fcb2cd15d0d2deaa995f71451382a
Nadia Animashaun <nerdyaha13c@gmail.com>
2026-02-07 13:03:05 -0500

    Update adc.h

--- firmware_c/adc.h
+++ firmware_c/adc.h
@@ -5,7 +5,6 @@
   Simplified support code for performing analog-to-digital conversion
   using the Companion Board for the DE0 or DE0-CV
 
-  N. Manjikian, ECE Dept., Queen's University, November 2018
   -------------------------------------------------------------------*/
 
 /* function prototypes for preparing then performing conversion */
@@ -15,3 +14,4 @@ extern void         InitADC (int adc_sel, int mux_sel);
 extern unsigned int ADConvert (void);
 
 #endif /* _ADC_H_ */
+
